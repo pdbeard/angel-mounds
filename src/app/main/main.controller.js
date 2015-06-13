7b902f97d344779763fefe8644b1835142b5a3d1
David M. Reagan <dmreagan@iu.edu>
2015-06-12 22:52:51 -0400

    Add filter to sites so that when they are clicked, the appropriate media appear around them. Media layout still needs some work. Need to add tests. #4. #5.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,4 +4,12 @@ angular.module('angelMounds')
   .controller('MainController', ['$scope', 'sites', 'media', function ($scope, sites, media) {
     $scope.sites = sites.query();
     $scope.media = media.query();
+    
+    $scope.x = function (index, length) {
+      return 150 * Math.cos(360 * index / length);
+    };
+    $scope.y = function (index, length) {
+      return 150 * Math.sin(360 * index / length);
+    };
+                    
   }]);
