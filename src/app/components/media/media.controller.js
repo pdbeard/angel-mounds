e2280117c1b72e2fa07916e64c3b303aed049379
pdbeard <pdbeard@iu.edu>
2015-07-13 09:59:53 -0400

    Add X3D Init and Material Design Lite Library.

--- src/app/components/media/media.controller.js
+++ src/app/components/media/media.controller.js
@@ -7,7 +7,7 @@ angular.module('angelMounds')
       START_ANGLE = 360 * ($scope.$index / $scope.filteredMedia.length),
       START_X = $scope.site.radius * Math.cos(START_ANGLE * (Math.PI / 180)),
       START_Y = $scope.site.radius * Math.sin(START_ANGLE * (Math.PI / 180));
-    
+
     // the current state of the media item
     $scope.transform = {
       translate: {
@@ -17,15 +17,15 @@ angular.module('angelMounds')
       scale: START_SCALE,
       angle: START_ANGLE
     };
-    
+
     // new transforms are applied relative to the old one
     oldTransform = $scope.transform;
-    
+
     // a grabbed item is raised above the other items
     $scope.grabThis = function () {
       $scope.grabbed = true;
     };
-    
+
     // update the item's transform based on the touch event
     $scope.touchThis = function ($event) {
       $scope.transform = {
@@ -37,10 +37,18 @@ angular.module('angelMounds')
         angle: oldTransform.angle + $event.rotation
       };
     };
-    
+
     // save the current transform
     $scope.cantTouchThis = function () {
       oldTransform = $scope.transform;
       $scope.grabbed = false;
     };
+
+    // Checks to see if
+    $scope.initX3D = function(){
+            x3dom.reload();
+
+    };
+
+
   }]);
