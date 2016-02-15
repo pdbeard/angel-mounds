d72c205ed0d49be8f122c4dc846f798e45cbbb27
pdbeard <pdbeard@iu.edu>
2016-02-15 14:05:28 -0500

    fix #47

--- src/app/components/slideable/slideable.directive.js
+++ src/app/components/slideable/slideable.directive.js
@@ -5,20 +5,26 @@ angular.module('angelMounds')
 	return {
 		restrict:'C',
 		compile: function (element, attr) {
-			// wrap tag
+			// Wrap tag
 			var contents = element.html();
 			element.html('<div class="slideable_content" style="" >' + contents + '</div>');
 
 			return function postLink(scope, element, attrs) {
-				// default properties
+				// Default properties
 				attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
 				attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+				var titleSize = element[0].firstChild.querySelector(".titlebox").scrollHeight;
+
+				console.log(titleSize); //returns correct size if more text is added, wrong size if one line
+
+				// Guessing I need the whole CSS style here so it doesn't get overridden.
 				element.css({
 					'overflow': 'hidden',
-//                  'overflow-y':'auto',
-					'height': '15%',
+//				    'overflow-y':'auto',
+//					'height': titleSize+"px",
+					'height': "359px",
 					'position':'absolute',
-					'top':'100%',
+//					'top':'100%',
 					'background-color':'white',
 					'color': '#000000',
 					'transitionProperty':'none',
@@ -31,56 +37,43 @@ angular.module('angelMounds')
 					'transform': 'translateY(-100%)'
 				});
 			};
+
 		}
 	};
 })
 .directive('slideToggle', function() {
 	return {
 		restrict: 'A',
-		link: function(scope, element, attrs) {
+		link: function(scope, element, attrs)
+		{
+			scope.clickIcon = 'info_outline';
+			scope.clickIconMorph = function()
+			{
+				if (scope.clickIcon === 'close') {
+					scope.clickIcon = 'info_outline';
+				}
+				else {
+					scope.clickIcon = 'close';
+				}
+			};
 
-		scope.clickIcon = 'info_outline';
-					scope.clickIconMorph = function() {
-					if (scope.clickIcon === 'close') {
-						scope.clickIcon = 'info_outline';
-					}
-					else {
-						scope.clickIcon = 'close';
-					}
-				};
-
-			var target, content;
-			attrs.expanded = false;
-			scope.slideHeight = .2;
-//          target.style.height = '20px';
-
-
-
-			element.bind('click', function() {
-
-				var height=(element[0].parentElement.parentElement.clientHeight);
-				if (!target) target = document.querySelector(attrs.slideToggle);
-				if (!content) content = target.querySelector('.slideable_content');
+			var target;
+			element.bind('click', function()
+			{
+				var height=(element[0].parentElement.clientHeight);
+				if (!target) target = element[0].parentElement.parentElement.parentElement;
 
 				target.style.transitionProperty ="height";
+				console.log(height);
 
-console.log(height);
 				if(!attrs.expanded) {
-//					target.style.height ="100%";
-					scope.slideHeight = 1;
-//					scope.arrowRotate = 180;
+					target.style.height ="100%";
 				} else {
-//					target.style.height ="auto";
-					scope.slideHeight = .2;
-//				    target.style.height = '20px';
-//					scope.arrowRotate = 0;
+					target.style.height = height +"px";
+
 				}
-
-
-
 				attrs.expanded = !attrs.expanded;
 				setTimeout(function(){target.style.transitionProperty="none"}, 300);
-
 			});
 		}
 	};
