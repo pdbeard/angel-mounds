2446dea97d62adaba07e33c5c88ae94d757bf559
pdbeard <pdbeard@iu.edu>
2016-02-22 15:34:28 -0500

    Add missing idle state code

--- src/app/index.js
+++ src/app/index.js
@@ -1,30 +1,37 @@
 'use strict';
 
 angular.module('angelMounds',
-               ['ngAnimate',
-                'ngMaterial',
+			   ['ngAnimate',
+				'ngMaterial',
 		'ngMdIcons',
-                'ngSanitize',
-                'ngResource',
-                'ui.router',
-                'hmTouchEvents',
-                'sitesFactory',
-                'mediaFactory',
-                'zIndexFactory',
-                'windowScaleFactory',
-                'layoutFactory',
-                'mediaFilter',
-                'trustedUrlFilter',
-                'angulartics',
-                'angulartics.google.analytics',
-                'ngIdle'])
+				'ngSanitize',
+				'ngResource',
+				'ui.router',
+				'hmTouchEvents',
+				'sitesFactory',
+				'mediaFactory',
+				'zIndexFactory',
+				'windowScaleFactory',
+				'layoutFactory',
+				'mediaFilter',
+				'trustedUrlFilter',
+				'angulartics',
+				'angulartics.google.analytics',
+				'ngIdle'])
   .config(function ($stateProvider, $urlRouterProvider, IdleProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController'
-      });
-  
-    $urlRouterProvider.otherwise('/');
+	$stateProvider
+	  .state('home', {
+		url: '/',
+		templateUrl: 'app/main/main.html',
+		controller: 'MainController'
+	  });
+
+	$urlRouterProvider.otherwise('/');
+
+	// configure idle settings
+	IdleProvider.idle(60);
+	IdleProvider.timeout(20);
+  })
+  .run(function (Idle) {
+	Idle.watch();
   });
