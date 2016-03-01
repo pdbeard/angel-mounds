674cb1a7ae5feb826e90f151a9802b938f10023a
pdbeard <pdbeard@iu.edu>
2016-03-01 16:12:34 -0500

    Fix zoom, Fix text highlight

--- src/app/components/drab/drab.directive.js
+++ src/app/components/drab/drab.directive.js
@@ -2,47 +2,58 @@
   'use strict';
 
   angular
-    .module('angelMounds')
-    .directive('drab', ['zIndex', drab]);
+	.module('angelMounds')
+	.directive('drab', ['zIndex', drab]);
 
   /** @ngInject */
   function drab(zIndex) {
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/drab/drab.html',
-      scope: {
-        config: '='
-      },
-      controller: DrabController,
-      controllerAs: 'dc',
-      bindToController: true,
-      link: link
-    };
+	var directive = {
+	  restrict: 'E',
+	  templateUrl: 'app/components/drab/drab.html',
+	  scope: {
+		config: '='
+	  },
+	  controller: DrabController,
+	  controllerAs: 'dc',
+	  bindToController: true,
+	  link: link
+	};
 
-    return directive;
-    
-    
-    function link(scope, element) {
-      /*element.children().css({
-        left: scope.dc.config.left,
-        bottom: scope.dc.config.bottom
-      });*/
-    }
+	return directive;
 
-    /**
-     * Controller
-     * initialize the item's transform
-     */ 
-    /** @ngInject */
-    function DrabController($scope) {
-      var dc = this;
-      
-      dc.active = false;
-      
-      dc.activate = function () {
-        dc.active = !dc.active;
-      };
-    }
+
+	function link(scope, element) {
+	  /*element.children().css({
+		left: scope.dc.config.left,
+		bottom: scope.dc.config.bottom
+	  });*/
+	}
+
+	/**
+	 * Controller
+	 * initialize the item's transform
+	 */
+	/** @ngInject */
+	function DrabController($scope) {
+	  var dc = this;
+
+	  dc.active = false;
+
+dc.frameLoad = function(){
+
+	  var iframe = document.getElementsByClassName('itemIframe');
+	  var frameBody = iframe[0].contentDocument.body;
+	  frameBody.click();
+		console.log(iframe[0].contentDocument.body);
+//	window.onload = function(){
+//		  //var iframeBody = iframe.contentWindow.document.body;
+//		  console.log("testetse");
+//}
+}
+	  dc.activate = function () {
+		dc.active = !dc.active;
+	  };
+	}
   }
 
 })();
